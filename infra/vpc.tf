306dc9096abfa84ab80d1437b18e7cb519447e82
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-09-08 22:55:37 +0100

    added security group rules

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -77,33 +77,34 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.ecs-vpc.id
 }
 # ALB SG Rules
-resource "aws_vpc_security_group_ingress_rule" "example" {
-  security_group_id = aws_security_group.example.id
+resource "aws_vpc_security_group_ingress_rule" "alb_inbound" {
+  security_group_id = aws_security_group.alb_sg.id
 
-  cidr_ipv4   = "10.0.0.0/8"
-  from_port   = 80
-  to_port     = 80
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  to_port     = 443
   ip_protocol = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "example" {
-  security_group_id = aws_security_group.example.id
+resource "aws_vpc_security_group_egress_rule" "alb_outbound" {
+  security_group_id = aws_security_group.alb_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
 }
-# ECS SG Rules
-resource "aws_vpc_security_group_ingress_rule" "example" {
-  security_group_id = aws_security_group.example.id
 
-  cidr_ipv4   = "10.0.0.0/8"
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
+# ECS SG Rules
+resource "aws_vpc_security_group_ingress_rule" "ecs_inbound" {
+  security_group_id = aws_security_group.ecs_sg.id
+
+  referenced_security_group_id = aws_security_group.alb_sg.id
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "example" {
-  security_group_id = aws_security_group.example.id
+resource "aws_vpc_security_group_egress_rule" "ecs_outbound" {
+  security_group_id = aws_security_group.ecs_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
