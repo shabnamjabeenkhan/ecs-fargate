8ae83a2fd5f1ad6474d1190ee6d4bc686dfdc4d2
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-09-13 00:48:00 +0100

    certificate issued by adding record in route53

--- infra/acm.tf
+++ infra/acm.tf
@@ -9,6 +9,31 @@ resource "aws_acm_certificate" "acm_cert" {
 
 
 # Route53
-# resource "aws_route53_zone" "primary" {
-#   name = "example.com"
-# }
+resource "aws_route53_zone" "threatmod_route53" {
+  name = "shabnamkhan.tech"
+}
+
+
+
+# ACM VAlidation
+resource "aws_route53_record" "acm_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.threatmod_route53.zone_id
+}
+
+resource "aws_acm_certificate_validation" "acm_validation" {
+  certificate_arn         = aws_acm_certificate.acm_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
+}
