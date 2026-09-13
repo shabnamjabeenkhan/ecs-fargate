9041317470449f2f4c504bd04bdecb5d1a40501e
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-09-13 01:00:48 +0100

    created alb listener

--- infra/alb.tf
+++ infra/alb.tf
@@ -10,15 +10,15 @@ resource "aws_lb" "ecs_alb" {
 
 
 # Create Listener
-# resource "aws_lb_listener" "front_end" {
-#   load_balancer_arn = aws_lb.front_end.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.acm_cert.arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.front_end.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+}
