efb0fc5ca81e7361644b409bee1ad5b732a2871f
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-09-15 01:22:49 +0100

    created diffenet modules

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,19 @@
 module "ecs" {
   source = "./modules/ecs"
 }
+
+module "ecs" {
+  source = "./modules/ecs"
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+module "acm" {
+  source = "./modules/acm"
+}
+
+module "alb" {
+  source = "./modules/alb"
+}
